14ad2baee343c61e85211eb26571f8f662c6d11d
TEKI VIJAYARATNAM <tekivijayaratnam12345@gmail.com>
2025-08-01 22:10:38 +0530

    Update RootController.java

--- src/main/java/com/busbooking/bus_reservation/controller/RootController.java
+++ src/main/java/com/busbooking/bus_reservation/controller/RootController.java
@@ -2,7 +2,8 @@ package com.busbooking.bus_reservation.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity; // ✅ Required import
+import org.springframework.http.HttpStatus; 
 @RestController
 public class RootController {
 
